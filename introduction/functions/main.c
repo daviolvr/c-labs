9f01a025541bb57e830590759d4eff5b53186d75
Davi de Oliveira <olidavi031130@gmail.com>
2025-06-29 16:16:13 -0300

    functions

---
+++ introduction/functions/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int foo(int bar) {
+    return bar * 2;
+}
+
+void print_big(int number);
+
+int main() {
+    // int result = foo(1);
+    // printf("%d\n", result);
+
+    // exercise
+    int array[] = { 1, 11, 2, 22, 3, 33 };
+    int i;
+    for (i = 0; i < 6; i++) {
+        print_big(array[i]);
+    }
+}
+
+void print_big(int number) {
+    if (number > 10) {
+        printf("%d is big\n", number);
+    }
+}
